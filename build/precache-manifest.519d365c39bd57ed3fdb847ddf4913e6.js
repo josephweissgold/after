a41aabb36a1ed1cea4ad6de68fedfb80954c5e01
josephweissgold <59490328+josephweissgold@users.noreply.github.com>
2020-04-28 15:13:25 -0400

    Production Build

---
+++ build/precache-manifest.519d365c39bd57ed3fdb847ddf4913e6.js
@@ -0,0 +1,130 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "de219c8566672403512b282dd5ef0e77",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4d8448d435b1c61bb3c9",
+    "url": "/static/css/2.69121389.chunk.css"
+  },
+  {
+    "revision": "a1d7bdd58555a1174f25",
+    "url": "/static/css/main.a3b42676.chunk.css"
+  },
+  {
+    "revision": "4d8448d435b1c61bb3c9",
+    "url": "/static/js/2.96a0d385.chunk.js"
+  },
+  {
+    "revision": "00ba9122ace4f7cfb2b78b0b307611b4",
+    "url": "/static/js/2.96a0d385.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a1d7bdd58555a1174f25",
+    "url": "/static/js/main.1a5de2fb.chunk.js"
+  },
+  {
+    "revision": "0a663fbde0de373e4cbd",
+    "url": "/static/js/runtime-main.845d25be.js"
+  },
+  {
+    "revision": "4482eb0628a7d802c9ac4a81aa5017eb",
+    "url": "/static/media/body1.4482eb06.jpg"
+  },
+  {
+    "revision": "aeeb5486d89122b8685a25d29a952b76",
+    "url": "/static/media/body2.aeeb5486.jpg"
+  },
+  {
+    "revision": "8949a7314506ae72cd48f5f8f4387f48",
+    "url": "/static/media/body3.8949a731.jpg"
+  },
+  {
+    "revision": "8d7fbaeb11c2f8bdf1cdc7bfdd917614",
+    "url": "/static/media/body4.8d7fbaeb.jpg"
+  },
+  {
+    "revision": "8f6a502d68f3af801b456b09575bad7f",
+    "url": "/static/media/body5.8f6a502d.jpg"
+  },
+  {
+    "revision": "e02337f03aee4b026a652d170e1d15b4",
+    "url": "/static/media/care1.e02337f0.jpg"
+  },
+  {
+    "revision": "3aba19234477e4ac979a1ee047a8aede",
+    "url": "/static/media/care2.3aba1923.jpg"
+  },
+  {
+    "revision": "ef4cf5d9ddf3a4b1124cf1ba2477989e",
+    "url": "/static/media/care3.ef4cf5d9.jpg"
+  },
+  {
+    "revision": "1aee980da8e8f3ab6537a3c4b77d7ce2",
+    "url": "/static/media/care4.1aee980d.jpg"
+  },
+  {
+    "revision": "a08da6ace43d702459cd151b5bab1c3b",
+    "url": "/static/media/care5.a08da6ac.jpg"
+  },
+  {
+    "revision": "7c7e99d42312f132c1cd050dd046eb09",
+    "url": "/static/media/ceremony1.7c7e99d4.jpg"
+  },
+  {
+    "revision": "35692caceb8b775f7eacf73800e0f3c0",
+    "url": "/static/media/ceremony2.35692cac.jpg"
+  },
+  {
+    "revision": "4ccc0811c0fb7f22ab946fc4ba761799",
+    "url": "/static/media/ceremony3.4ccc0811.jpg"
+  },
+  {
+    "revision": "1e664a66925adde19e6aa3767767cbad",
+    "url": "/static/media/ceremony4.1e664a66.jpg"
+  },
+  {
+    "revision": "e3f6a8f603f2328e98e32573d38dbf66",
+    "url": "/static/media/ceremony5.e3f6a8f6.jpg"
+  },
+  {
+    "revision": "31f279178498547b3224b4738746f288",
+    "url": "/static/media/legacy1.31f27917.jpg"
+  },
+  {
+    "revision": "854395278c519a971db33a20429c56fd",
+    "url": "/static/media/legacy2.85439527.jpg"
+  },
+  {
+    "revision": "604a57a925dbb0a29c1e6c502cabf0b0",
+    "url": "/static/media/legacy3.604a57a9.jpg"
+  },
+  {
+    "revision": "aecde09873ab028d88752d12852b9551",
+    "url": "/static/media/legacy4.aecde098.jpg"
+  },
+  {
+    "revision": "162c329231be543ec5e570537a73b939",
+    "url": "/static/media/legacy5.162c3292.jpg"
+  },
+  {
+    "revision": "3ed36b1807339f6d08a2ce5424989ad6",
+    "url": "/static/media/treasures1.3ed36b18.jpg"
+  },
+  {
+    "revision": "7c4dd319836d514d75bb15bf68734b3c",
+    "url": "/static/media/treasures2.7c4dd319.jpg"
+  },
+  {
+    "revision": "bbfb655ff356541d2b8891cf50c8d931",
+    "url": "/static/media/treasures3.bbfb655f.jpg"
+  },
+  {
+    "revision": "41fc052963524d90f4b7c322222f2441",
+    "url": "/static/media/treasures4.41fc0529.jpg"
+  },
+  {
+    "revision": "2533c285dd2a7e6edded854abe4ae6ba",
+    "url": "/static/media/treasures5.2533c285.jpg"
+  }
+]);
